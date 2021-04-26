fb46c6ef4ae0f41bbed469d5286ba1d80427a508
Hercules Gabriel <herculesgabriel00@gmail.com>
2021-04-26 18:08:42 -0300

    chore: add config base files

---
+++ src/routes.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+
+const routes = Router();
+
+routes.route('/')
+  .get((request, response) => {
+    response
+      .status(200)
+      .json({ message: 'OK' })
+  });
+
+export { routes };
